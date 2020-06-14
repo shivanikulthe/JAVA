c89740dea58e9869edd640cf34d141c34ca4a922
shivanikulthe <61390077+shivanikulthe@users.noreply.github.com>
2020-06-15 00:02:05 +0530

    Add files via upload

---
+++ AS_2.java
@@ -0,0 +1,31 @@
+public class AS_2
+{
+pulic static void int main(string[] args)
+{
+ch='a';
+switch(ch)
+{
+case a:
+system.out.pritln("is vowel");
+break;
+case A:system.out.pritln("is vowel");
+break;
+case e:system.out.pritln("is vowel");
+break;
+case E:system.out.pritln("is vowel");
+break;
+case i:system.out.pritln("is vowel");
+break;
+case I:system.out.pritln("is vowel");
+break;
+case o:system.out.pritln("is vowel");
+break;
+case O:system.out.pritln("is vowel");
+break;
+case u:system.out.pritln("is vowel");
+break;
+case U:system.out.pritln("is vowel");
+break;
+}
+}
+}
