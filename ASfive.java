9b4bca237357917431137f88252e6716be0d61c9
shivanikulthe <61390077+shivanikulthe@users.noreply.github.com>
2020-06-15 00:03:37 +0530

    Add files via upload

---
+++ ASfive.java
@@ -0,0 +1,27 @@
+ class ASfive
+{
+public static void main(string[] args)
+{
+int mks=55;
+if(mks<35)
+{
+system.out.println("fail");
+}
+else if(mks>35)
+{
+system.out.println("pass");
+}
+else if(mks>50)
+{
+system.out.println("second class");
+}
+else if(mks>60)
+{d
+system.out.println("first class");
+}
+else
+{
+system.out.println("destinction");
+}
+}
+}
